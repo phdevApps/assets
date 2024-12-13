0c2ddfe58e247e6340af1de0c8ac950d59341856
phdevApps <phdev.work@gmail.com>
2024-12-13 07:28:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // clean up
 [...document.querySelectorAll('link')].filter(it=>{
     return (!it.className.match(/style_1/ig) && (it.innerHTML.match(/blogger/ig) || it.href.match(/blogger/ig)))
-}).forEach(it=>it.remove())
+}).forEach(it=>it.remove());
 [...document.querySelectorAll('script')].filter(it=>{
     return (!it.className.match(/script_1/ig) && (it.innerHTML.match(/blogger/ig) || it.src.match(/blogger/ig)))
-}).forEach(it=>it.remove())
-document.querySelectorAll('[class*="section"]').forEach(it=>it.remove())
-document.querySelectorAll('style[id*="page-skin"][type="text/css"]').forEach(it=>it.remove())
-document.querySelectorAll('meta[name*="google-adsense"]').forEach(it=>it.remove())
+}).forEach(it=>it.remove());
+document.querySelectorAll('[class*="section"]').forEach(it=>it.remove());
+document.querySelectorAll('style[id*="page-skin"][type="text/css"]').forEach(it=>it.remove());
+document.querySelectorAll('meta[name*="google-adsense"]').forEach(it=>it.remove());
