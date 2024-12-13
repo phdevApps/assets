491e9d5e8095b297cd2547ddf4054d35a5d2c5db
phdevApps <phdev.work@gmail.com>
2024-12-13 07:09:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('hi');
+// clean up
+
+[...document.querySelectorAll('script')].filter(it=>{
+    return (!it.className.match(/script_1/ig) && (it.innerHTML.match(/blogger/ig) || it.src.match(/blogger/ig)))
+}).forEach(it=>it.remove())
+document.querySelectorAll('[class*="section"]').forEach(it=>it.remove())
+document.querySelectorAll('style[id*="page-skin"][type="text/css"]').forEach(it=>it.remove())
+document.querySelectorAll('meta[name*="google-adsense"]').forEach(it=>it.remove())
