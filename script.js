6aa8624e1f3af2034995b221b33c92fbad69834d
phdevApps <phdev.work@gmail.com>
2025-01-06 14:41:03 +0200

    save new tmp

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// clean up
-document.all[0].getAttributeNames().forEach(it=>it!='lang'?document.all[0].removeAttribute(it):'');
-[...document.querySelectorAll('link')].filter(it=>{
-    return (!it.className.match(/style_1/ig) && (it.innerHTML.match(/blogger/ig) || it.href.match(/blogger/ig)))
-}).forEach(it=>it.remove());
-[...document.querySelectorAll('script')].filter(it=>{
-    return (!it.className.match(/script_1/ig) && (it.innerHTML.match(/blogger/ig) || it.src.match(/blogger/ig)))
-}).forEach(it=>it.remove());
-document.querySelectorAll('[id="placeholder2rm"]').forEach(it=>it.remove());
-document.querySelectorAll('style[id*="page-skin"][type="text/css"]').forEach(it=>it.remove());
-document.querySelectorAll('meta[name*="google-adsense"]').forEach(it=>it.remove());
+(function() {
+    var target = document.currentScript; 
+    window.addEventListener('load', function() {
+      calendar.schedulingButton.load({
+        url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0dzpn2O7_S9BcwW3oek945uyBiDZE4hNSyMvPOuWv5pE7oGeEXju6EolNdrSwDb7TaiHBaz9q?gv=true', 
+        color: '#039BE5',
+        label: 'Book a Meeting', 
+        target,
+      });
+    });
+  })();
